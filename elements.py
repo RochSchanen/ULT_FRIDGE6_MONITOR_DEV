1c9be6c9b919f86ea7506688e9470f6c71a7039d
Roch Schanen <r.schanen@lancaster.ac.uk>
2025-04-27 20:17:47 +0100

    add Push

--- elements.py
+++ elements.py
@@ -15,9 +15,14 @@ _DESCRIPTION = """
 _HISTORY = {
 
     "0.00": """
-        add display()
+        add display():
     """,
-    
+
+    "0.01": """
+        add _btn(), Push():
+
+    """,
+
     }
 
 #####################################################################
@@ -48,6 +53,8 @@ if _debug.flag('TESTS'):
 #####################################################################
 #                                                         ### IMPORTS
 # from wxpython: https://www.wxpython.org/
+
+# Display
 from wx import Control                  as _wxControl
 from wx import ID_ANY                   as _wxID_ANY
 from wx import DefaultPosition          as _wxDefaultPosition
@@ -58,8 +65,18 @@ from wx import EVT_ERASE_BACKGROUND     as _wxEVT_ERASE_BACKGROUND
 from wx import EVT_PAINT                as _wxEVT_PAINT
 from wx import BufferedPaintDC          as _wxBufferedPaintDC
 
+# _btn
+from wx import EVT_LEFT_DOWN            as _wxEVT_LEFT_DOWN
+from wx import EVT_LEFT_DCLICK          as _wxEVT_LEFT_DCLICK
+from wx import PostEvent                as _wxPostEvent
+from wx.lib.newevent import NewEvent    as _wxNewEvent
+
+# Push
+from wx import EVT_LEFT_UP              as _wxEVT_LEFT_UP
+from wx import EVT_LEAVE_WINDOW         as _wxEVT_LEAVE_WINDOW
+
 #####################################################################
-#                                                         ### LIBRARY
+#                                                         ### Display
 class Display(_wxControl):
     
     # superseed __init__()
@@ -119,6 +136,95 @@ class Display(_wxControl):
 
     def GetValue(self):
         return self.status
+
+#####################################################################
+#                                                            ### _btn
+class _btn(Display):
+
+    def __init__(self, parent, images, names = None):
+
+        Display.__init__(self, parent, images, names)
+
+        # locals
+        self.radio = None
+        self.ctr   = None
+        self.evt   = None
+
+        # bindings
+        self.Bind(_wxEVT_LEFT_DOWN,   self._onMouseDown)
+
+        # capture double clicks events as secondary single clicks
+        self.Bind(_wxEVT_LEFT_DCLICK, self._onMouseDown)
+
+        # call child _start() method
+        self._start()
+
+        # done
+        return
+
+    def _start(self):
+        pass
+
+    # radio feature
+    def _clear(self):
+        pass
+
+    # on EVT_LEFT_DOWN, the event.skip()
+    # method must be called to preserve
+    # the focus event to be processed
+    def _onMouseDown(self, event):
+        event.Skip() # allow focus events
+        return
+
+    # Bind the event to the parent handler
+    def BindEvent(self, handler):
+        # "handler" is a reference to the handler method
+        # usually defined by the parent class
+        self.ctr, self.evt = _wxNewEvent()
+        self.GetParent().Bind(self.evt, handler)
+        return
+
+    # Sends a event to parent using "status" as parameter
+    def SendEvent(self):
+        if self.ctr:
+            event = self.ctr(caller=self, status=self.status)
+            _wxPostEvent(self.GetParent(), event)
+        return
+
+#####################################################################
+#                                                            ### Push
+# (send event on pressing)
+class Push(_btn):
+
+    def _start(self):
+        self.lock = False
+        self.Bind(_wxEVT_LEFT_UP,      self._onMouseUp)
+        self.Bind(_wxEVT_LEAVE_WINDOW, self._onMouseLeave)
+        return
+
+    def _onMouseDown(self, event):
+        event.Skip() # allow focus events
+        self.lock = True
+        if self.radio:
+            self.radio.Select(self)
+        self.status = 1
+        self.Refresh()
+        self.SendEvent()
+        return
+
+    def _onMouseUp(self, event):
+        if self.lock:
+            self.lock = False
+            self.status = 0
+            self.Refresh()
+        return
+
+    def _onMouseLeave(self, event):
+        if self.lock:
+            self.lock = False
+            self.status = 0
+            self.Refresh()
+        return
 
 #####################################################################
 #                                                           ### TESTS
@@ -181,3 +287,52 @@ if __name__ == "__main__":
             m.Run()
 
             _log.print(" . end test for version 0.00")
+
+        if _test.flag('0.01'):
+            
+            _log.print(" . running test for version 0.01")
+
+            from base   import app
+            from theme  import images
+
+            # derive a new class from app
+            class myapp(app):
+
+                def led1_handler(self, event):
+                    _log.print(f'led1 pressed...')
+                    return
+
+                def Start(self):
+
+                    # instanciate image library
+                    library = images()
+                    
+                    # load background bitmap (all of the images)
+                    library.load('bkgd')
+
+                    # use background bitmap
+                    self.Panel.BackgroundBitmap = library.select('bkgd')
+                    
+                    # load led bitmaps (blue and red only)
+                    library.load('leds', ['red', 'blue'])
+
+                    led1 = Push(
+                        self.Panel,
+                        library.select(
+                            'leds', 
+                            'red', 
+                            ['on', 'off'],
+                            ),
+                        )
+
+                    led1.BindEvent(self.led1_handler)
+
+                    led1.SetPosition((100, 100))
+                    
+                    # done
+                    return
+            
+            m = myapp()
+            m.Run()
+
+            _log.print(" . end test for version 0.01")
